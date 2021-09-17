b735cbe9b8406e78545c21b4cb96abb1ecbb8156
Anthony Piccinone <anthony.piccinone@gmail.com>
2021-09-17 19:04:20 -0400

    Moved event path to a DEFINE preprocessor

--- typewriter.c
+++ typewriter.c
@@ -6,11 +6,13 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+#define DEVICE "/dev/input/event18"
+
 int main(int argc, char **argv) {
     struct input_event event;
 
     //Find event name in /proc/bus/input/devices
-    int keyboard = open("/dev/input/event18", O_RDONLY);
+    int keyboard = open(DEVICE, O_RDONLY);
     if(keyboard == -1) {
         fprintf(stderr, "Could not open keyboard");
     }
